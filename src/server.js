4132dd32dd3aab2d4cf174c4a21fcb18a2c712ec
jenniferkissinger <jenn.michiko@gmail.com>
2019-06-19 14:37:13 -0400

    installed mongodb and mongoose and set up basic User constructor

--- src/server.js
+++ src/server.js
@@ -1,3 +1,40 @@
 // TODO
+const mongoose = require('mongoose');
+
+// Connect to database (connectionURL, options object)
+mongoose.connect('mongodb://127.0.0.1:27017/players-api', {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+
+// User constructor
+const User = mongoose.model('User', {
+  id: {
+    type: String
+  },
+  first_name: {
+    type: String
+  },
+  last_name: {
+    type: String
+  },
+  email: {
+    type: String
+  }
+});
+
+const me = new User({
+  id: '1',
+  first_name: 'Jenn',
+  last_name: 'K',
+  email: 'name@email.com'
+});
+
+me.save().then(() => {
+  console.log(me);
+}).catch((error) => {
+  console.log('Error', error)
+});
+
 
 module.exports = {};
