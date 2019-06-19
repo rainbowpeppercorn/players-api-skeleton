c695f2965b8957a90404c9c6ac69e3e3fb32bbf5
jenniferkissinger <jenn.michiko@gmail.com>
2019-06-19 19:33:40 -0400

    modified existing api calls to reflect async/await functionality

--- src/index.js
+++ src/index.js
@@ -11,84 +11,92 @@ app.use(express.json());
 
 
 // Create a User
-app.post('/api/user', (req, res) => {
+app.post('/api/user', async (req, res) => {
   const user = new User(req.body);
 
-  user.save().then(() => {
+  try {
+    await user.save();
+    // If all goes well...
     res.status(201).send(user);
-  }).catch((e) => {
+  } catch (e) {
     res.status(400).send(e);
-  });
+  }
 });
 
 // Get array of all Users stored in DB
-app.get('/api/user', (req, res) => {
-  // Returns an array of all users
-    User.find({}).then((users) => {
-      res.send(users);
-    }).catch((e) => {
-      res.status(500).send();
-    });
+app.get('/api/user', async (req, res) => {
+  try {
+    const users = await User.find({});
+    
+    res.send(users);
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
 // Get individual User by ID
-app.get('/api/user/:id', (req, res) => {
+app.get('/api/user/:id', async (req, res) => {
   const _id = req.params.id;
 
-  User.findById(_id).then((user) => {
+  try {
+    const user = await User.findById(_id);
+
     // Check if such a User exists
     if (!user) {
       return res.status(404).send();
     }
 
-    // If all goes well... get dat User
     res.send(user);
-  }).catch((e) => {
+  } catch (e) {
     // Was getting CastErrors when testing IDs that were not mongoose-approved
     if (e.name === 'CastError') {
       return res.status(404).send();
     }
 
     res.status(500).send();
-  });
+  }
 });
 
 // Create a Player
-app.post('/api/players', (req, res) => {
+app.post('/api/players', async (req, res) => {
   const player = new Player(req.body)
 
-  player.save().then(() => {
+  try {
+    await player.save();
     res.status(201).send(player);
-  }).catch((e) => {
+  } catch (e) {
     res.status(400).send(e);
-  });
+  }
 });
 
-// Get (Read) all Players
-app.get('/api/players', (req, res) => {
-  // Returns an array of all players
-  Player.find({}).then((tasks) => {
-    res.send(tasks);
-  }).catch((e) => {
+// Get (Read) an array of all Players
+app.get('/api/players', async (req, res) => {
+  try {
+    const players = await Player.find({});
+    res.send(players);
+  } catch (e) {
     res.status(500).send();
-  });
+  }
 });
 
 // Get (Read) single Player by ID
-app.get('/api/players/:id', (req, res) => {
+app.get('/api/players/:id', async (req, res) => {
   const _id = req.params.id;
 
-  Player.findById(_id).then((player) => {
+  try {
+    const player = await Player.findById(_id);
+
     // Check if such a player exists
     if (!player) {
       return res.status(404).send();
     }
-
+  
     // If all goes well... get dat player
     res.send(player);
-  }).catch((e) => {
+
+  }  catch (e) {
     res.status(500).send();
-  });
+  }
 });
 
 app.listen(port, () => {
