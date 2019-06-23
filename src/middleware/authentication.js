1afecde49c0d6aadf0fc82b766d774afec3d3323
jenniferkissinger <jenn.michiko@gmail.com>
2019-06-23 18:06:11 -0400

    added a bunch of console.log()'s to debug

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -2,13 +2,18 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 const auth = async (req, res, next) => {
+
   console.log('top of the mw');
+
   try {
     if (!req.header('Authorization')) {
+
       console.log('inside if statement mw');
+
       return res.status(403).send('Auth token not found');
     }
     console.log('inside middleware');
+
     const token = req.header('Authorization').replace('Bearer ', ''); // Remove Bearer from the front of the jwt string
     const decoded = jwt.verify(token, 'secretcodesupersecret'); // decoded will have the _id property
 
