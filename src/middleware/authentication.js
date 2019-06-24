5c77488468e2e69394c301572ea3b49fc51f44f3
jenniferkissinger <jenn.michiko@gmail.com>
2019-06-24 16:04:34 -0400

    removed console.logs, for some post-debugging clean up

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -3,31 +3,24 @@ const User = require('../models/user');
 
 const auth = async (req, res, next) => {
 
-  console.log('top of the mw');
-
   try {
+    // Check for authentication token 
     if (!req.header('Authorization')) {
-
-      console.log('inside if statement mw');
-
       return res.status(403).send('Auth token not found');
     }
-    console.log('inside middleware');
 
     const token = req.header('Authorization').replace('Bearer ', ''); // Remove Bearer from the front of the jwt string
     const decoded = jwt.verify(token, 'secretcodesupersecret'); // decoded will have the _id property
-
     const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
 
-    console.log('after user middleware');
-
+    // Check both user and token exist
     if(!user || !token) {
       throw new Error();
     }
 
     // If all goes well...
-    req.token = token; // Add the token toooo
-    req.user = user; // Add the user on for the route handler 
+    req.token = token; // Set token property 
+    req.user = user; // Set user property  
     next();
 
   } catch (e) {
