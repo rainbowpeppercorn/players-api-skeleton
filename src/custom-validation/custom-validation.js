8118736699ccb2ff218ca1858c247258b622e3c3
jenniferkissinger <jenn.michiko@gmail.com>
2019-06-24 17:43:15 -0400

    beefed up password validation using password-validator

--- src/custom-validation/custom-validation.js
+++ src/custom-validation/custom-validation.js
@@ -1,10 +1,28 @@
+const validator = require('validator');
+const passwordValidator = require('password-validator');
+
+const passwordSchema = new passwordValidator();
+passwordSchema
+.is().min(8)
+.has().digits()
+.has().not().spaces()
+.is().not().oneOf(['Passw0rd', 'passw0rd', 'Password123', 'password123']);
+
 const customValidation = {
   validatePassword: (value) => {
     if (value.toLowerCase().includes('password')) {
-      throw new Error('Password cannot contain the word password');
+      throw new Error('User password cannot contain the word password');
     }
+    if (!passwordSchema.validate(value)) {
+      throw new Error('User password must be at least 8 characters, containing both letters and numbers.');
+    }
+
   },
-  validateEmail: ''
+  validateEmail: (value) => {
+    if (!validator.isEmail(value)) {
+      throw new Error('Email is invalid');
+    }
+  }
 }
 
 
