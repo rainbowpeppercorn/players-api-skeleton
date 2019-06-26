2b472f8146e4704f26011a12c5fb0b5df374ae3e
jenniferkissinger <jenn.michiko@gmail.com>
2019-06-26 13:05:42 -0400

    added PATCH call to update a partial player and fixed PUT call

--- src/routers/player.js
+++ src/routers/player.js
@@ -3,11 +3,11 @@ const Player = require('../models/player');
 const auth = require('../middleware/authentication');
 const router = new express.Router();
 
-// Create a Player
+// CREATE A PLAYER
 router.post('/api/players', auth, async (req, res) => {
   // Add the owner ID to any new Player
  const player = new Player({
-   ...req.body, // ES6 spread operator : copies all props from body over to new Player
+   ...req.body, // ES6 spread operator : copies all props from req.body over to new Player
    created_by: req.user._id
  });
 
@@ -22,17 +22,17 @@ router.post('/api/players', auth, async (req, res) => {
     res.status(201).send(response);
   } catch (e) {
     if (!req.body.first_name || !req.body.last_name || !req.body.rating || !req.body.handedness) {
-      return res.status(409).send('Please provide all Player info.');
+      return res.status(409).send('A complete Player profile requires a full name, rating, and handedness.');
     }
 
-    res.status(409).send(e);
+    res.status(409).send(e.message);
   }
 });
 
-// Get all Players (for logged in User only)
+// GET ALL PLAYERS (for logged in User only)
 router.get('/api/players', auth, async (req, res) => {
   try {
-    await req.user.populate('players').execPopulate();
+    await req.user.populate('players').execPopulate(); // populate() == mongoose's pseudo-join
 
     const response = {
       success: true,
@@ -41,16 +41,15 @@ router.get('/api/players', auth, async (req, res) => {
 
     res.send(response);
   } catch (e) {
-    res.status(500).send();
+    res.status(500).send(e.message);
   }
 });
 
-// Get (Read) single Player by ID
+// GET PLAYER BY ID
 router.get('/api/players/:id', auth, async (req, res) => {
   const _id = req.params.id;
 
   try {
-
     const player = await Player.findOne({ _id: _id, created_by: req.user._id });
 
     // Check if such a player exists
@@ -62,8 +61,31 @@ router.get('/api/players/:id', auth, async (req, res) => {
     res.send(player);
 
   }  catch (e) {
-    res.status(500).send();
+    res.status(500).send(e.message);
   }
+});
+
+// UPDATE ENTIRE PLAYER BY ID
+router.put('/api/players/:id', auth, async (req, res) => {
+
+  try {
+    const player = await Player.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!player) {
+      return res.status(404).send();
+    }
+
+    await player.save();
+
+    const response = {
+      player,
+      success: true
+    };
+
+    res.send(response);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+
 });
 
 // Update a Player by ID
@@ -104,14 +126,14 @@ router.delete('/api/players/:id', auth, async (req, res) => {
     const player = await Player.findOneAndDelete({ _id: req.params.id, created_by: req.user._id });
 
     if (!player) {
-      return res.status(404).send();
+      return res.status(404).send('This player does not exist');
     }
 
     // If successful, send back the deleted player's info
     res.send(player);
 
   } catch (e) {
-    res.status(404).send('player does not exist');
+    res.status(404).send(e.message);
   }
 });
 
