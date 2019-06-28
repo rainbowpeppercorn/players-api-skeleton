ac5453447a954a2c624adf08303faf03a3371abe
jenniferkissinger <jenn.michiko@gmail.com>
2019-06-28 01:47:39 -0400

    threw in some console.logs for debugging then took them back out

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -11,15 +11,16 @@ const auth = async (req, res, next) => {
     const token = req.header('Authorization').replace('Bearer ', ''); // Remove Bearer from the front of the jwt string
     const decoded = jwt.verify(token, process.env.JWT_SECRET); // Decoded will have the _id property ()
     const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
-
+  
     // Check both user and token exist
     if(!user || !token) {
       throw new Error('Either user or token not found');
     }
 
     // If all goes well...
-    req.token = token; // Set token property 
-    req.user = user; // Set user property  
+    req.token = token; // Set token property on req
+    req.user = user; // Set user property on req
+    
     next();
 
   } catch (e) {
