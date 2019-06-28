9fe3c08ba33fa1c4746d5a1c87b4a7898e057fbc
jenniferkissinger <jenn.michiko@gmail.com>
2019-06-28 01:48:48 -0400

    attached confirm password field to model hashtag whoops and toyed with idea of leaving off id field... kept it tho

--- src/models/user.js
+++ src/models/user.js
@@ -66,7 +66,7 @@ userSchema.virtual('players', {
 // Generate JWT
 userSchema.methods.generateAuthToken = async function () {
   const user = this; 
-  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET, { expiresIn: '90 days' });
 
   // Add user's token to the user object and save to DB
   user.tokens = user.tokens.concat({ token: token });
